a6aeebde95e8ea10c3c99313512e9143fd073cfe
David-barce <71525685+David-barce@users.noreply.github.com>
2021-09-22 17:55:53 -0500

    Initial commit

---
+++ App3/ViewModels/NombreViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace App3.ViewModels
+{
+    public class NombreViewModel : ViewModelBase
+    {
+        private string nom;
+        private string ap1;
+        private string ap2;
+
+        public string Nombre
+        {
+            get => nom;
+            set
+            {
+                if (String.Equals(nom, value)) return;
+                nom = value;
+                RaisePropertyChanged(nameof(Nombre));
+                RaisePropertyChanged(nameof(NomCompleto));
+            }
+
+        }
+
+        public string ApPaterno
+        {
+            get => ap1;
+            set
+            {
+                if (String.Equals(ap1, value)) return;
+                ap1 = value;
+                RaisePropertyChanged(nameof(ApPaterno));
+                RaisePropertyChanged(nameof(NomCompleto));
+            }
+
+        }
+
+        public string ApMaterno
+        {
+            get => ApMaterno;
+            set
+            {
+                if (String.Equals(ap2, value)) return;
+                ap2 = value;
+                RaisePropertyChanged(nameof(ApMaterno));
+                RaisePropertyChanged(nameof(NomCompleto));
+            }
+
+        }
+
+        public string NomCompleto
+        {
+            get => string.Format("Tu nombre es: {0} {1} {2}", nom, ap1, ap2);
+        }
+
+    }
+}
---
+++ App3/ViewModels/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using App3.ViewModels;
+
+namespace App3.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged(string nombrePropiedad)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nombrePropiedad));
+        }
+
+    }
+
+}
